6e836e1ac9701048a8e147bfd5f53ea902677e3a
gangagyatso4364 <gyatsokhan@gmail.com>
2023-10-13 16:00:51 +0530

    web_rewuest_script

---
+++ src/umed_modern_filter/web_request.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def get_ttl(work_id):
+    try:
+        ttl = requests.get(f"http://purl.bdrc.io/graph/M{work_id}.ttl")
+        return ttl.text
+    except Exception as e:
+        print(" TTL not Found!!!", e)
+        return None
+
+
+print(get_ttl("W0IA_HOD35"))
